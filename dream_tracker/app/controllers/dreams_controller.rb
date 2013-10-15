485f9c1a9bd35b7e57332ab6aaf5b97c62323d25
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-15 16:45:42 -0400

    Routes working! forgot to commit more :(

--- dream_tracker/app/controllers/dreams_controller.rb
+++ dream_tracker/app/controllers/dreams_controller.rb
@@ -1,6 +1,41 @@
 class DreamsController < ApplicationController
 	before_action :set_dream, only: [:show, :edit, :update, :destroy]
 
+	def show
+	end
+
+	def index
+		@dreams = Dream.all
+	end
+
+	def new
+		@dream = Dream.new
+	end
+
+	def create
+		@dream = Dream.new(dream_params)
+		if @dream.save
+			redirect_to @dream
+		else
+			render action: 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @dream.update(dream_params)
+			redirect_to @dream
+		else
+			render action: 'edit'
+		end
+	end
+
+	def destroy
+		@dream.destroy
+		redirect_to dreams_url
+	end
 
 	private
 
@@ -9,6 +44,6 @@ class DreamsController < ApplicationController
 	end
 
 	def dream_params
-		params.require(:dream).permit(:)
+		params.require(:dream).permit(:topic, :body, :image_url, :rating, :scary, :Freudian)
 	end
 end
--- dream_tracker/db/seeds.rb
+++ dream_tracker/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Dream.create(topic: "running", body: "I was running a race but then my shoes came untied and I couldn't move my feet anymore and all the spectators ignored me.", rating: '3', scary: true, Freudian: false, image_url: 'http://www.thesportinmind.com/wp-content/uploads/2013/05/ws_Girl_Running_1680x1050.jpg')
+Dream.create(topic: "witches", body: "This girl from my high school who I don't like much was chasing me on a broomstick. I think I was flying.", rating: '4', scary: false, Freudian: false, image_url: 'http://cinefilles.files.wordpress.com/2013/06/practical_magic_original.jpg')
+Dream.create(topic: "Bon Me", body: "I was setting up at Dewey and none of the rice cookers were working and we had a huge line right at 11.", rating: '5', scary: true, Freudian: false, image_url: 'http://1.bp.blogspot.com/-QTPbGpHfDb8/Tf5lKnIGYrI/AAAAAAAAQpY/8HNXO3DpzYg/s1600/Bon%2BMe%2BFood%2BTruck%2B011.JPG')
+Dream.create(topic: "track", body: "I was running the 600 at Woburn and that Woburn girl with the tall ponytail tried to pass me but I beat her the last lap.", rating: '8', scary: false, Freudian: false, image_url: 'http://www.thesportinmind.com/wp-content/uploads/2013/05/ws_Girl_Running_1680x1050.jpg')
